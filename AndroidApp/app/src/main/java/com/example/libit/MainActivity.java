029f4ed6ea193220492b396929bbe460952054d9
Stepan <stepansmetanskyy@gmail.com>
2020-10-04 10:10:09 +0300

    add changes

--- AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.libit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.android.volley.toolbox.NetworkImageView;
 import com.example.libit.network.ImageRequester;
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageRequester = ImageRequester.getInstance();
         editImage = findViewById(R.id.chooseImage);
-        imageRequester.setImageFromUrl(editImage,"https://www.monsterenergy.com/media/uploads_image/2019/09/09/1600/800/6ea650cb9a77f4ac03b1c87fd8b21429.jpg?mod=v1_0485ae83b33a11d39f357c0a1ad1802f");
+        imageRequester.setImageFromUrl(editImage,"http://10.0.2.2:53558/images/testAvatarHen.jpg");
+    }
+
+    public void btnLogin(View v) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
