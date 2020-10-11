49ca1828d99503aa7517ab04af6644d2a5965560
Den-droid <amerscan8@gmail.com>
2020-10-11 09:04:49 +0300

    commit changes

--- AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
@@ -7,8 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.toolbox.NetworkImageView;
-import com.example.libit.entities.Currency;
-import com.example.libit.entities.Post;
 import com.example.libit.network.ImageRequester;
 import com.example.libit.network.NetworkService;
 
@@ -19,7 +17,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private ImageRequester imageRequester;
     private NetworkImageView editImage;
@@ -35,53 +32,5 @@ public class MainActivity extends AppCompatActivity {
         imageRequester.setImageFromUrl(editImage,baseUrl+"images/belka.jpg");
 
         final TextView textView = findViewById(R.id.textView);
-
-//        NetworkService.getInstance()
-//                .getJSONApi()
-//                .getPostWithID(1)
-//                .enqueue(new Callback<Post>() {
-//                    @Override
-//                    public void onResponse(@NonNull Call<Post> call, @NonNull Response<Post> response) {
-//                        Post post = response.body();
-//
-//                        textView.append(post.getId() + "\n");
-//                        textView.append(post.getUserId() + "\n");
-//                        textView.append(post.getTitle() + "\n");
-//                        textView.append(post.getBody() + "\n");
-//                    }
-//
-//                    @Override
-//                    public void onFailure(@NonNull Call<Post> call, @NonNull Throwable t) {
-//
-//                        textView.append("Error occurred while getting request!");
-//                        t.printStackTrace();
-//                    }
-//                });
-
-                NetworkService.getInstance()
-                .getJSONApi()
-                .getCurrency()
-                .enqueue(new Callback<List<Currency>>() {
-                    @Override
-                    public void onResponse(@NonNull Call<List<Currency>> call, @NonNull Response<List<Currency>> response) {
-                        List<Currency> currencies = response.body();
-                        for (Currency currency : currencies){
-                            textView.append(currency.getCcy() + "\n");
-                            textView.append(currency.getBase_ccy() + "\n");
-                            textView.append(currency.getBuy() + "\n");
-                            textView.append(currency.getSale() + "\n");
-
-                            textView.append("\n\n");
-                        }
-
-                    }
-
-                    @Override
-                    public void onFailure(@NonNull Call<List<Currency>> call, @NonNull Throwable t) {
-
-                        textView.append("Error occurred while getting request!");
-                        t.printStackTrace();
-                    }
-                });
     }
 }
--- AndroidApp/app/src/main/java/com/example/libit/network/NetworkService.java
+++ AndroidApp/app/src/main/java/com/example/libit/network/NetworkService.java
@@ -5,7 +5,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkService {
     private static NetworkService mInstance;
-//    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
     private static final String BASE_URL = "https://api.privatbank.ua/p24api/";
     private Retrofit mRetrofit;
 
