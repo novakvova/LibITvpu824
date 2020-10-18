8c4a3d8c52e9f3dab6dc28e0707b3fc9dcbd3f65
Stepan <stepansmetanskyy@gmail.com>
2020-10-19 00:27:24 +0300

    add menu (drawer layout)

--- AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/libit/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.libit;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.android.volley.toolbox.NetworkImageView;
 import com.example.libit.network.ImageRequester;
 import com.example.libit.network.NetworkService;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private ImageRequester imageRequester;
     private NetworkImageView editImage;
     private final String BASE_URL = NetworkService.getBaseUrl();
 
+    private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         imageRequester = ImageRequester.getInstance();
         editImage = findViewById(R.id.chooseImage);
         imageRequester.setImageFromUrl(editImage, BASE_URL + "/images/testAvatarHen.jpg");
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void btnLogin(View v) {
